6a144c429928ef29ac7ff6f8d79c61e8ddcb4580
WoodsKing <2465681755@qq.com>
2021-04-20 10:04:44 +0800

    略有改动

--- basis/random_function.py
+++ basis/random_function.py
@@ -1,5 +1,5 @@
-import random
 # 随机random模块
+
 """
 seed()                          初始化随机种子
 random()                        生成[0.0,1.0)只见随机数
@@ -10,7 +10,9 @@ uniform(a,b)                    生成[a,b]间随机小数
 choice(sep)                     从序列类型sep，随机返回元素
 shuffle(sep)                    将序列随机排列，返回打乱后的序列
 sample(pop,k)                   从pop类型中随机选取k个元素返回
+
 """
+import random
 
 list1 = 'qwertyuiop'
 print(random.sample(list1,5))
