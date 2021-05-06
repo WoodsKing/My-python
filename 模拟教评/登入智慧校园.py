a91808bf611eb4b6784df9e31c398e2940a957d7
WoodsKing <2465681755@qq.com>
2021-05-06 15:30:59 +0800

    提交代码

--- 模拟教评/登入智慧校园.py
+++ 模拟教评/登入智慧校园.py
@@ -1,23 +1,23 @@
 import requests
 import time
 
-username = '1903021662' #学号
-password = '2465681755mjy'           #密码
+username = '1903021662'  # 学号
+password = '2465681755mjy'  # 密码
 headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                   'Chrome/89.0.4389.128 Safari/537.36 ',
     'Host': 'ossc.cqcet.edu.cn'
 }
 data = {
-    'type':1,
+    'type': 1,
     'username': username,
     'password': password,
     'rememenber': 'on',
-    'img_code':'',
+    'img_code': '',
 }
 s = requests.Session()
-s.get('http://sso.cqcet.edu.cn/login',headers=headers)
+s.get('http://sso.cqcet.edu.cn/login', headers=headers)
 time.sleep(0.1)
-r = s.post('http://sso.cqcet.edu.cn/uaa/login_process',data=data)
+r = s.post('http://sso.cqcet.edu.cn/uaa/login_process', data=data)
 time.sleep(0.1)
 print(r.request.headers)
